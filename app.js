0ff9c8ff85f29b3b81f56bf1e3451a1089a613f5
Robihamanto <rrobie44@gmail.com>
2017-07-31 18:36:27 +0700

    Add CRU

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var url = require('url');
 var router = require('routes')();
+var qString = require('querystring');
 var view = require('swig');
 var mysql = require('mysql');
 var connection = mysql.createConnection({
@@ -64,15 +65,71 @@ router.addRoute('/home', function (req, res) {
 });
 
 router.addRoute('/register', function (req, res) {
-    var html = view.compileFile('./view/register.html')({
-        title: 'Register',
-    });
     if (req.method.toUpperCase() == 'POST') {
-
+        var data_post = '';
+        req.on('data', function (chuncks) {
+            data_post += chuncks;
+        });
+        req.on('end', function () {
+            data_post = qString.parse(data_post);
+            connection.query('INSERT INTO MAHASISWA SET ?', {
+                nim: data_post.nim,
+                nama: data_post.nama,
+            }, function (err, field) {
+                if (err) throw err;
+                res.writeHead(302, { 'Location': '/home' });
+                res.end();
+            });
+        });
     } else {
+        var html = view.compileFile('./view/register.html')({
+            title: 'Register',
+        });
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end(html);
     }
+});
+
+router.addRoute('/edit/:id?', function (req, res) {
+    connection.query('SELECT * FROM MAHASISWA WHERE ? ', {
+        id: this.params.id,
+    }, function (err, rows, field) {
+        if (err) throw err;
+        if (rows.length) {
+            var data = rows[0];
+            if (req.method.toUpperCase() == 'POST') {
+                var data_post = '';
+                req.on('data', function (chuncks) {
+                    data_post += chuncks;
+                });
+                req.on('end', function () {
+                    data_post = qString.parse(data_post);
+                    res.end();
+                    connection.query('UPDATE MAHASISWA SET ? WHERE ?', [{
+                            nama: data_post.nama,
+                        }, {
+                            id: data_post.id,
+                        }], function (err, field) {
+                        if (err) throw err;
+                        res.writeHead(302, { 'Location': '/home' });
+                        res.end();
+                    });
+                });
+
+            } else {
+                var html = view.compileFile('./view/edit.html')({
+                    person: data,
+                });
+            }
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(html);
+        } else {
+            var html = view.compileFile('./view/404.html');
+            res.writeHead(404, { 'Content-type': 'text/html' });
+            res.end(html);
+        }
+    });
+
 });
 
 http.createServer(function (req, res) {
